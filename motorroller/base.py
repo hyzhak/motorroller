ccfe5e9f8d892e15d5dce77db8e3fcd00492b959
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-01-06 01:00:38 +0100

    WIP: partly implemented

---
+++ motorroller/base.py
@@ -0,0 +1,5 @@
+__all__ = ('UPDATE_OPERATORS')
+
+UPDATE_OPERATORS = set(['set', 'unset', 'inc', 'dec', 'pop', 'push',
+                        'push_all', 'pull', 'pull_all', 'add_to_set',
+                        'set_on_insert', 'min', 'max'])
